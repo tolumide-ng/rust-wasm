09ebaf9968e8ba1442e1a7809fa1b365a92ddf56
tolumide-ng <tolumideshopein@gmail.com>
2022-06-10 18:20:43 +0100

    Wasm in wasm

--- wasm_in_wasm/src/lib.rs
+++ wasm_in_wasm/src/lib.rs
@@ -1,8 +1,50 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use js_sys::{Function, Object, Reflect, WebAssembly};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::{spawn_local, JsFuture};
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+const WASM: &[u8] = include_bytes!("add.wasm");
+
+async fn run_async() -> Result<(), JsValue> {
+    console_log("instantiating a new wasm module directly");
+
+    let a = JsFuture::from(WebAssembly::instantiate_buffer(WASM, &Object::new())).await?;
+    let b: WebAssembly::Instance = Reflect::get(&a, &"instance".into())?.dyn_into()?;
+
+    let c = b.exports();
+
+    let add = Reflect::get(c.as_ref(), &"add".into())?
+        .dyn_into::<Function>()
+        .expect("add export wasn't a function");
+
+    let three = add.call2(&JsValue::undefined(), &1.into(), &2.into())?;
+    console_log("1 + 2 = {:?}", three);
+
+    let mem = Reflect::get(c.as_ref(), &"memory".into())?
+        .dyn_into::<WebAssembly::Memory>()
+        .expect("memory export wasn't a `WebAssembly.Memory`");
+
+    console_log!("created module has {} pages of memory", mem.grow(0));
+    console_log!("giving the module 4 more pages of memory");
+    mem.grow(4);
+    console_log("now the moedule has {} pages of memory", mem.grow(0));
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn run() {
+    spawn_local(async {
+        run_async().await.unwrap_throw();
+    })
 }
