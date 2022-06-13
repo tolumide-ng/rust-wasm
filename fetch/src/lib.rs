cc4bb13b6cb684dcb9ac59a19987d8f5350cdae2
tolumide-ng <tolumideshopein@gmail.com>
2022-06-13 05:19:44 +0100

    Fetch request in rust triggered by js after it's compilation to wasm

--- fetch/src/lib.rs
+++ fetch/src/lib.rs
@@ -1,8 +1,56 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Signature {
+    pub name: String,
+    pub email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CommitDetails {
+    pub author: Signature,
+    pub committer: Signature,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Commit {
+    pub sha: String,
+    pub commit: CommitDetails,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Branch {
+    pub name: String,
+    pub commit: Commit,
+}
+
+
+#[wasm_bindgen]
+pub async fn run(repo: String) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = format!("https://api.github.com/repos/{}/branches/master", repo);
+    
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    request.headers().set("Accept", "application/vnd.github.v3+json")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let json = JsFuture::from(resp.json()?).await?;
+
+    let branch_info = json.into_serde::<Branch>().unwrap();
+
+    Ok(JsValue::from_serde(&branch_info).unwrap())
 }
